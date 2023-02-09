782f4e6ef7a630284b446d298d64bf32525db0b8
Ciberian <isi04047891@gmail.com>
2023-02-09 22:25:44 +0500

    Создаёт компонент SiteFooter

--- project/src/pages/items/catalog-page/catalog-page.tsx
+++ project/src/pages/items/catalog-page/catalog-page.tsx
@@ -1,4 +1,5 @@
 import SiteHeader from '../../../components/page-components/site-header/site-header';
+import SiteFooter from '../../../components/page-components/site-footer/site-footer';
 
 function CatalogPage(): JSX.Element {
   return (
@@ -7,7 +8,7 @@ function CatalogPage(): JSX.Element {
 
       <main></main>
 
-      <footer></footer>
+      <SiteFooter />
     </>
   );
 }
